55ba62476c2c4495e4f19e54ab774262095ed632
Morazz <morozsasha20820@gmail.com>
2021-12-17 00:43:20 +0300

    Templates adding and editing fixed

--- CPI/ClientApp/src/app/admin/coupon-template/add-template/add-template.component.ts
+++ CPI/ClientApp/src/app/admin/coupon-template/add-template/add-template.component.ts
@@ -1,6 +1,5 @@
 import { Time } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Template } from '@angular/compiler/src/render3/r3_ast';
 import { Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -9,20 +8,22 @@ import { Router } from '@angular/router';
   templateUrl: './add-template.component.html',
   styleUrls: ['./add-template.component.css']
 })
-/** add-template component*/
-export class AddTemplateComponent {
-  template: CouponTemplate = new CouponTemplate(new Date());
 
-  constructor(private router: Router, private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) { }
+export class AddTemplateComponent {
+  template: string = "";
+
+  constructor(private router: Router, private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {}
 
   postCoupon() {
-    console.log(this.template.time);
-    this.http.post(this.baseUrl + 'coupontemplate', this.template)
-      .subscribe(
-        result => {
-          this.router.navigate(['/templates-list']);
-        }, error => console.log(error));
-  };
+    const hours = this.template.substr(0, 2);
+    const minutes = this.template.substr(3, 2);
+    const date = new Date(Date.UTC(0, 0, 0, Number(hours), Number(minutes)));
+      this.http.post(this.baseUrl + 'coupontemplate', new CouponTemplate(date))
+        .subscribe(
+          result => {
+            this.router.navigate(['/templates-list']);
+          }, error => console.log(error));
+  }
 }
 
 export class CouponTemplate {
--- CPI/ClientApp/src/app/admin/coupon-template/edit-template/edit-template.component.ts
+++ CPI/ClientApp/src/app/admin/coupon-template/edit-template/edit-template.component.ts
@@ -1,4 +1,6 @@
-﻿import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'app-edit-template',
@@ -7,8 +9,40 @@
 })
 /** edit-template component*/
 export class EditTemplateComponent {
-    /** edit-template ctor */
-    constructor() {
+  template: string = "";
+  t_id: number;
+  temp: CouponTemplate = new CouponTemplate(0, new Date());
 
-    }
+  constructor(private router: Router, private activateRoute: ActivatedRoute, private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+    this.t_id = activateRoute.snapshot.params['template_id'];
+    this.getTemplate()
+  }
+
+  postCoupon() {
+    const hours = this.template.substr(0, 2);
+    const minutes = this.template.substr(3, 2);
+    const date = new Date(Date.UTC(0, 0, 0, Number(hours), Number(minutes)));
+    this.temp.time = date;
+    console.log(this.temp);
+
+    this.http.put(this.baseUrl + 'coupontemplate', this.temp)
+      .subscribe(
+        result => {
+          this.router.navigate(['/templates-list']);
+        }, error => console.log(error));
+  }
+
+  getTemplate() {
+    this.http.get<CouponTemplate>(this.baseUrl + 'coupontemplate/' + this.t_id)
+      .subscribe(
+        result => {
+          this.temp = result;
+        }, error => console.log(error));
+  }
+}
+
+export class CouponTemplate {
+  constructor(
+    public template_id: number,
+    public time: Date) { }
 }
