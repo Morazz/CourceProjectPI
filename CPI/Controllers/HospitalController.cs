4ae53dfee28cb3b81faf9b09f80da393585837c2
Morazz <morozsasha20820@gmail.com>
2022-04-27 13:23:53 +0300

    Hpspital cotroller added

---
+++ CPI/Controllers/HospitalController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CPI.Models;
+
+namespace CPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HospitalController : ControllerBase
+    {
+        DBContext db;
+
+        public HospitalController(DBContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Hospital> Get()
+        {
+            return db.Hospitals.ToArray();
+        }
+
+        [HttpGet("{id}")]
+        public Hospital Get(string id)
+        {
+            return db.Hospitals.Find(id);
+        }
+
+        [HttpPost]
+        public void Insert(Hospital hospital)
+        {
+            db.Hospitals.Add(hospital);
+            db.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void Delete(string id)
+        {
+            Hospital hospital = Get(id);
+            if (hospital != null)
+            {
+                db.Hospitals.Remove(hospital);
+                db.SaveChanges();
+            }
+        }
+
+        [HttpPut]
+        public void Update(Hospital newHospital)
+        {
+            Hospital hospital = Get(newHospital.hospital_id);
+            if (hospital != null)
+            {
+                hospital.name = newHospital.name;
+                hospital.address = newHospital.address;
+                hospital.schedule = newHospital.schedule;
+                db.Hospitals.Update(hospital);
+                db.SaveChanges();
+            }
+        }
+    }
+}
